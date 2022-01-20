f4b78ca61c6f435aa6fed131fddb5dedea5242c6
Ryzee119 <wendland@live.com.au>
2022-01-20 14:39:33 +1030

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+TinyUSB host dev on Teensy41
+
+Build with [VSCode](https://code.visualstudio.com/) and the [PlatformIO](https://platformio.org/) plugin.
+
+* Build with (✓) on the PlatformIO toolbar.
+* Program with (→) on the PlatformIO toolbar.
---
+++ src/main.cpp
@@ -0,0 +1,110 @@
+#include <Arduino.h>
+#include "tusb.h"
+
+void usbh_hw_init();
+
+void setup()
+{
+    Serial1.begin(256000);
+    usbh_hw_init();
+    tusb_init();
+}
+
+void loop()
+{
+    tuh_task();
+}
+
+#ifdef CFG_TUSB_DEBUG_PRINTF
+char outbuf[256];
+extern "C" int CFG_TUSB_DEBUG_PRINTF(const char *format, ...)
+{
+    va_list args;
+    va_start(args, format);
+    vsnprintf(outbuf, sizeof(outbuf), format, args);
+    Serial1.print(outbuf);
+    //Serial1.printf(format, args);
+    Serial1.flush();
+    return 1;
+}
+#endif
+
+void USB_OTG2_IRQHandler(void)
+{
+    tuh_int_handler(1);
+}
+
+void usbh_hw_init()
+{
+    // Teensy 4.0 PLL & USB PHY powerup
+    while (1)
+    {
+        uint32_t n = CCM_ANALOG_PLL_USB2;
+        if (n & CCM_ANALOG_PLL_USB2_DIV_SELECT)
+        {
+            CCM_ANALOG_PLL_USB2_CLR = 0xC000; // get out of 528 MHz mode
+            CCM_ANALOG_PLL_USB2_SET = CCM_ANALOG_PLL_USB2_BYPASS;
+            CCM_ANALOG_PLL_USB2_CLR = CCM_ANALOG_PLL_USB2_POWER |
+                                      CCM_ANALOG_PLL_USB2_DIV_SELECT |
+                                      CCM_ANALOG_PLL_USB2_ENABLE |
+                                      CCM_ANALOG_PLL_USB2_EN_USB_CLKS;
+            continue;
+        }
+        if (!(n & CCM_ANALOG_PLL_USB2_ENABLE))
+        {
+            CCM_ANALOG_PLL_USB2_SET = CCM_ANALOG_PLL_USB2_ENABLE; // enable
+            continue;
+        }
+        if (!(n & CCM_ANALOG_PLL_USB2_POWER))
+        {
+            CCM_ANALOG_PLL_USB2_SET = CCM_ANALOG_PLL_USB2_POWER; // power up
+            continue;
+        }
+        if (!(n & CCM_ANALOG_PLL_USB2_LOCK))
+        {
+            continue; // wait for lock
+        }
+        if (n & CCM_ANALOG_PLL_USB2_BYPASS)
+        {
+            CCM_ANALOG_PLL_USB2_CLR = CCM_ANALOG_PLL_USB2_BYPASS; // turn off bypass
+            continue;
+        }
+        if (!(n & CCM_ANALOG_PLL_USB2_EN_USB_CLKS))
+        {
+            CCM_ANALOG_PLL_USB2_SET = CCM_ANALOG_PLL_USB2_EN_USB_CLKS; // enable
+            continue;
+        }
+        break; // USB2 PLL up and running
+    }
+    // turn on USB clocks (should already be on)
+    CCM_CCGR6 |= CCM_CCGR6_USBOH3(CCM_CCGR_ON);
+    // turn on USB2 PHY
+    USBPHY2_CTRL_CLR = USBPHY_CTRL_SFTRST | USBPHY_CTRL_CLKGATE;
+    USBPHY2_CTRL_SET = USBPHY_CTRL_ENUTMILEVEL2 | USBPHY_CTRL_ENUTMILEVEL3;
+    USBPHY2_PWD = 0;
+
+#ifdef ARDUINO_TEENSY41
+    IOMUXC_SW_MUX_CTL_PAD_GPIO_EMC_40 = 5;
+    IOMUXC_SW_PAD_CTL_PAD_GPIO_EMC_40 = 0x0008; // slow speed, weak 150 ohm drive
+    GPIO8_GDIR |= 1 << 26;
+    GPIO8_DR_SET = 1 << 26;
+#endif
+    attachInterruptVector(IRQ_USB2, USB_OTG2_IRQHandler);
+}
+
+void tuh_hid_report_received_cb(uint8_t dev_addr, uint8_t instance, uint8_t const *report, uint16_t len)
+{
+    TU_LOG1("Got data: ");
+    TU_LOG1_MEM(report, len, 4);
+    tuh_hid_receive_report(dev_addr, instance);
+}
+
+void tuh_hid_umount_cb(uint8_t dev_addr, uint8_t instance)
+{
+    TU_LOG1("HID UNMOUNTED %02x %d\n", dev_addr, instance);
+}
+
+void tuh_hid_mount_cb(uint8_t dev_addr, uint8_t instance, uint8_t const *desc_report, uint16_t desc_len)
+{
+    TU_LOG1("HID MOUNTED %02x %d\n", dev_addr, instance);
+}
