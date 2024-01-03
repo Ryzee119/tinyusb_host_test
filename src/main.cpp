1e4f48e6156ec99b9b899cb7a62e49734504541e
Ryan Wendland <wendland@live.com.au>
2024-01-03 15:14:59 +1030

    Updates

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,11 @@
 #include <Arduino.h>
-#include<stdarg.h>
+#include <stdarg.h>
 #include "tusb.h"
+#include "xinput_host.h"
 
 #define DEBUG_MAX_LINES 16
-#define DEBUG_MAX_LEN 128
-static char outbuf[32][DEBUG_MAX_LEN];
+#define DEBUG_MAX_LEN 256
+static char outbuf[DEBUG_MAX_LEN];
 
 void usbh_hw_init();
 
@@ -18,15 +19,7 @@ void setup()
 void loop()
 {
     tuh_task();
-    for (int i = 0; i < DEBUG_MAX_LINES; i++)
-    {
-        if (outbuf[i][0] != 0)
-        {
-            Serial1.print(outbuf[i]);
-            Serial1.flush();
-            outbuf[i][0] = 0;
-        }
-    }
+    yield();
 }
 
 
@@ -36,14 +29,8 @@ extern "C" int CFG_TUSB_DEBUG_PRINTF(const char *format, ...)
     //This can be in IRQ context. Store message and output to user later
     va_list args;
     va_start(args, format);
-    for (int i = 0; i < DEBUG_MAX_LINES; i++)
-    {
-        if (outbuf[i][0] == 0)
-        {
-            vsnprintf(outbuf[i], DEBUG_MAX_LEN, format, args);
-            break;
-        }
-    }
+    vsnprintf(outbuf, DEBUG_MAX_LEN, format, args);
+    Serial1.print(outbuf);
     return 1;
 }
 #endif
@@ -51,6 +38,11 @@ extern "C" int CFG_TUSB_DEBUG_PRINTF(const char *format, ...)
 void USB_OTG2_IRQHandler(void)
 {
     tuh_int_handler(1);
+}
+
+usbh_class_driver_t const* usbh_app_driver_get_cb(uint8_t* driver_count){
+    *driver_count = 1;
+    return &usbh_xinput_driver;
 }
 
 void usbh_hw_init()
@@ -108,6 +100,7 @@ void usbh_hw_init()
     GPIO8_GDIR |= 1 << 26;
     GPIO8_DR_SET = 1 << 26;
 #endif
+
     attachInterruptVector(IRQ_USB2, USB_OTG2_IRQHandler);
 }
 
@@ -126,4 +119,50 @@ void tuh_hid_umount_cb(uint8_t dev_addr, uint8_t instance)
 void tuh_hid_mount_cb(uint8_t dev_addr, uint8_t instance, uint8_t const *desc_report, uint16_t desc_len)
 {
     TU_LOG1("HID MOUNTED %02x %d\n", dev_addr, instance);
+}
+
+void tuh_xinput_report_received_cb(uint8_t dev_addr, uint8_t instance, uint8_t const *report, uint16_t len)
+{
+    xinputh_interface_t *xid_itf = (xinputh_interface_t *)report;
+    xinput_gamepad_t *p = &xid_itf->pad;
+    const char* type_str;
+    switch (xid_itf->type)
+    {
+        case 1: type_str = "Xbox One";          break;
+        case 2: type_str = "Xbox 360 Wireless"; break;
+        case 3: type_str = "Xbox 360 Wired";    break;
+        case 4: type_str = "Xbox OG";           break;
+        default: type_str = "Unknown";
+    }
+
+    if (xid_itf->connected && xid_itf->new_pad_data)
+    {
+        TU_LOG1("[%02x, %02x], Type: %s, Buttons %04x, LT: %02x RT: %02x, LX: %d, LY: %d, RX: %d, RY: %d\n",
+             dev_addr, instance, type_str, p->wButtons, p->bLeftTrigger, p->bRightTrigger, p->sThumbLX, p->sThumbLY, p->sThumbRX, p->sThumbRY);
+
+        //How to check specific buttons
+        if (p->wButtons & XINPUT_GAMEPAD_A) TU_LOG1("You are pressing A\n");
+    }
+    tuh_xinput_receive_report(dev_addr, instance);
+}
+
+void tuh_xinput_mount_cb(uint8_t dev_addr, uint8_t instance, const xinputh_interface_t *xinput_itf)
+{
+    TU_LOG1("XINPUT MOUNTED %02x %d\n", dev_addr, instance);
+    // If this is a Xbox 360 Wireless controller we need to wait for a connection packet
+    // on the in pipe before setting LEDs etc. So just start getting data until a controller is connected.
+    if (xinput_itf->type == XBOX360_WIRELESS && xinput_itf->connected == false)
+    {
+        tuh_xinput_receive_report(dev_addr, instance);
+        return;
+    }
+    tuh_xinput_set_led(dev_addr, instance, 0, true);
+    tuh_xinput_set_led(dev_addr, instance, 1, true);
+    tuh_xinput_set_rumble(dev_addr, instance, 0, 0, true);
+    tuh_xinput_receive_report(dev_addr, instance);
+}
+
+void tuh_xinput_umount_cb(uint8_t dev_addr, uint8_t instance)
+{
+    TU_LOG1("XINPUT UNMOUNTED %02x %d\n", dev_addr, instance);
 }
